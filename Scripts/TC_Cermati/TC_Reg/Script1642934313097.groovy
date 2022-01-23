07c652b345ba3fd7941d213c812005ee8b89fb69
Hekmatyar114 <gbbheks11@gmail.com>
2022-01-23 17:47:06 +0700

    AutomationCermati

--- Scripts/TC_Cermati/TC_Reg/Script1642934313097.groovy
+++ Scripts/TC_Cermati/TC_Reg/Script1642934313097.groovy
@@ -17,3 +17,36 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.cermati.com/gabung-v2?')
+
+WebUI.setText(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/input__email'), 'gbbheks11@gmail.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/input__password'), 
+    'vJbJxW87ldozRbWrqehnww==')
+
+WebUI.click(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/div_Daftar ke Cermati.comEmail Kata Sandi K_39e489'))
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/input__confirmPassword'), 
+    'vJbJxW87ldozRbWrqehnww==')
+
+WebUI.click(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/div_Nama Depan'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/input__firstName'), 'Ghulbuddin')
+
+WebUI.setText(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/input__lastName'), 'Hikmatiar')
+
+WebUI.setText(findTestObject('Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/input__mobilePhone'), '085624011101')
+
+WebUI.setText(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/input__residenceCity'), 
+    'Kota Bogor')
+
+WebUI.delay(5)
+
+WebUI.click(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/div_KOTA BOGOR'))
+
+WebUI.click(findTestObject('Object Repository/Page_Bandingkan Kartu Kredit, Pinjaman, Tab_5f1b86/button_Daftar Akun'))
+
+WebUI.closeBrowser()
+
